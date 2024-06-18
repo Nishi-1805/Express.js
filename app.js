bf0793f4879429e3fa1bdc19d13bca5fbdd86f0f
Nishi-1805 <nishimishra180500@gmail.com>
2024-06-18 21:37:53 +0530

    commit changes

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
-app.use(shopRoutes);
+app.use('/admin', adminRoutes);
 
-app.listen(3000);
+app.use('/shop', shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>');
+});
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
+
