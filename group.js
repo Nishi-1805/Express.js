522a8375a7475deb80dd7c62987c5cad2610f453
Nishi-1805 <165827696+Nishi-1805@users.noreply.github.com>
2024-06-19 18:19:41 +0530

    Basic Group Chat Application

--- group.js
+++ group.js
@@ -1,55 +1,50 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const app = express();
+const PORT = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
 
-app.get('/login', (req, res)=>{
+let messages = [];
+
+app.post('/send-message', (req, res) => {
+    const { username, message } = req.body;
+    messages.push({ username, message });
+
+    fs.writeFile(path.join(__dirname, 'messages.json'), JSON.stringify(messages, null, 2))
+        .then(() => {
+            console.log('Message stored successfully');
+            res.json({ success: true });
+        })
+        .catch(error => {
+            console.error('Error storing message:', error);
+            res.status(500).json({ error: 'Error storing message' });
+        });
+});
+
+app.get('/messages', (req, res) => {
+   
+    fs.readFile(path.join(__dirname, 'messages.json'), 'utf8')
+        .then(data => {
+            messages = JSON.parse(data);
+            res.json(messages);
+        })
+        .catch(error => {
+            console.error('Error reading messages:', error);
+            res.status(500).json({ error: 'Error reading messages' });
+        });
+});
+
+app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'login.html'));
 });
 
-app.post('/login', (req, res)=>{
-    const {username} = req.body;
-    res.redirect('/');
-})
-
-app.get('/', (req, res)=>{
-    const username = localStorage.getItem('username');
-    if(!username){
-        res.redirect('/login');
-    }
-    else{
-        res.sendFile(path.join(__dirname, 'chat.html'));
-    }; 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.post('/send', (req, res)=>{
-    const {username, message} = req.body;
-    const newMessage = {username, message};
-
-    fs.readFile('message.json', (err, data)=>{
-        let messages = [];
-        if(!err){
-            messages = JSON.parse(data);
-        }
-        messages.push(newMessage);
-        fs.writeFile('message.json', JSON.stringify(messages), (err)=>{
-            if(err) throw err;
-        })
-    })
-    res.redirect('/');
-})
-app.get('/messages', (req, res)=>{
-    fs.readFile('message.json', (err, data)=>{
-        if (err){
-            return res.join([]);
-        };
-        res.join(JSON.parse(data));
-    });
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
-app.listen(3000,()=>{
-    console.log('Server is running on localhost:3000');
-})
