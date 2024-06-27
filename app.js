56dcbd65cc0693865592718f198ff7c108ef9d59
Nishi-1805 <nishimishra180500@gmail.com>
2024-06-27 09:19:12 +0530

    changes done

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
-const fs = require('fs');
-
+const sequelize = require('./util/database');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -21,33 +20,16 @@ app.use('/' ,shopRoutes);
 app.use(successRoutes);
 app.use(cartRoutes); 
 
-const cartFilePath = path.join(__dirname, 'data', 'cart.json');
-const initialCartData = { products: [], totalPrice: 0 };
-
-// Check if cart.json exists, if not, create it with initial data
-if (!fs.existsSync(cartFilePath)) {
-    fs.writeFileSync(cartFilePath, JSON.stringify(initialCartData), 'utf-8', (err) => {
-        if (err) {
-            console.error('Error creating cart.json file:', err);
-        }
-    });
-}
-
-const dataDir = path.join(__dirname, 'data');
-const dataFilePath = path.join(dataDir, 'products.json');
-
-if (!fs.existsSync(dataDir)) {
-    fs.mkdirSync(dataDir);
-}
-
-if (!fs.existsSync(dataFilePath)) {
-    fs.writeFileSync(dataFilePath, '[]', 'utf-8');
-}
-
 // Error handling middleware
 app.use(errorController.get404);
 
-const port = process.env.PORT || 7000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+sequelize
+.sync()
+.then(result=>{
+    console.log(result);
+    app.listen(7000);
+})
+.catch(err=>{
+    console.log(err);
+})
+
